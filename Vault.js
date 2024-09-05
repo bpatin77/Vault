e555ab0e50a10c0b6ea67d5a278174c826db3d5e
Blake <bpatin77@gmail.com>
2024-09-04 20:30:59 -0500

    Vault Done

---
+++ Vault.js
@@ -0,0 +1,6 @@
+const x=5+5; //This code is the first variable which will equal 10 using the + operator
+const y=2*20; //This code is the second variable which will equal 40 using the * operator
+const z=40-1; //This code is the third variable which will equal 39 using the - operator
+// const is a declaration that declares block-scoped local variables. 
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${x},${y},${z}`);
+//the above code will create a pop-up screen in the browser displaying the message within the () along with the secret combination of 10-40-39
